43110f623d5002d724cc8842bb38d584498133a8
Hershal Satam <hsatam@umich.edu>
2020-08-09 21:59:08 -0400

    Updates

---
+++ precache-manifest.51b93679150f0ac3071be680a6654fed.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb5e4413378ed934cc5c4e8bb7e1e79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0233c765af6f8cc7279d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "55f846173518ceb4ed82",
+    "url": "/static/css/main.53dc4a3f.chunk.css"
+  },
+  {
+    "revision": "0233c765af6f8cc7279d",
+    "url": "/static/js/2.6861acfc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.6861acfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55f846173518ceb4ed82",
+    "url": "/static/js/main.718f695e.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "b095d033c7d08eb67f2be0ff0fa157b2",
+    "url": "/static/media/Hershal_Satam_Resume.b095d033.pdf"
+  },
+  {
+    "revision": "efb70cb52ff38300db5a2b1deeacb55b",
+    "url": "/static/media/about1.efb70cb5.jpg"
+  },
+  {
+    "revision": "6295470a90fb4d529c4e464ba64c5d0a",
+    "url": "/static/media/about2.6295470a.jpg"
+  },
+  {
+    "revision": "47abd17cdda153cc4bce125c96c0d16e",
+    "url": "/static/media/about3.47abd17c.jpg"
+  },
+  {
+    "revision": "31df242cdedb44a4f70df87ac26a2535",
+    "url": "/static/media/about4.31df242c.jpg"
+  },
+  {
+    "revision": "a8e3c1a438adeef0cdb17106038bee77",
+    "url": "/static/media/about5.a8e3c1a4.jpg"
+  },
+  {
+    "revision": "6dff2a6dadbeafd8759b4ab2f5da6aed",
+    "url": "/static/media/about7.6dff2a6d.jpg"
+  },
+  {
+    "revision": "d54636e44b9a6996125229f732d8b397",
+    "url": "/static/media/introPi.d54636e4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.9fe250e89bac97af3e51904a0ae5cf1a.js"
+  "/precache-manifest.51b93679150f0ac3071be680a6654fed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
